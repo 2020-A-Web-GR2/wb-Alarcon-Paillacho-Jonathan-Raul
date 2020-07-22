f204e73f016640fc16719b4d5456b3559c3190e5
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-07-22 00:12:50 -0500

    Operaciones listas

--- 02-Deber/deber/src/deber/deber.controller.ts
+++ 02-Deber/deber/src/deber/deber.controller.ts
@@ -1,53 +1,178 @@
-import {Body, Controller, Delete, Get, Headers, HttpCode, Param, Post, Put, Query} from "@nestjs/common";
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Headers,
+    HttpCode,
+    Param,
+    Post,
+    Put,
+    Query
+} from "@nestjs/common";
+import {NumeroCreateDto} from "./dto/numero.create-dto";
+import {validate} from "class-validator";
 
 @Controller('inicio')
 
 //1:14:07
 
 export class DeberController {
+
     @Get ('suma/:segundo')
     @HttpCode(200)
-    suma(
+    async suma(
         @Query() num1,
         @Param() num2
     ){
-        console.log('primero',num1);
-        console.log('segundo',num2);
-        return 'Bienvenido, todo salió bien';
+        console.log("INICIA LA SUMA")
+        const primero = Number(num1.suma)
+        //console.log(typeof(primero))
+        const segundo = Number(num2.segundo)
+
+        const numeroValido = new NumeroCreateDto();
+        numeroValido.numero1 = primero;
+        numeroValido.numero2 = segundo;
+
+        try {
+            const existenErrores = await validate(numeroValido) //con el await siempre se utiliza el bloque try y catch
+            if(existenErrores.length > 0){
+                console.log('Errores: ',existenErrores);
+                throw new BadRequestException('Error validando');
+            } else {
+                const numero1 = Number(num1.suma);
+                const numero2 = Number(num2.segundo);
+                console.log(numero1)
+                console.log(numero2)
+                return "La suma de los dos números es: " + (numero1 + numero2);
+            }
+
+        }catch (e) {
+            console.error('Error', e);
+            throw new BadRequestException('Error validando');
+        }
+        //console.log('primero',num1);
+        //console.log('segundo',num2);
+        //return 'Bienvenido, todo salió bien';
     }
 
     @Put('resta')
     @HttpCode(201)
-    resta(
+    async resta(
         @Body() num1,
         @Query() num2
     ){
-        console.log('primero',num1.primero);
-        console.log('segundo',num2);
-        return 'Bienvenido resta, todo salió bien';
+        console.log("INICIA LA RESTA")
+        const primero = Number(num1.primero)
+        //console.log(typeof(primero))
+        const segundo = Number(num2.segundo)
+
+        const numeroValido = new NumeroCreateDto();
+        numeroValido.numero1 = primero;
+        numeroValido.numero2 = segundo;
+
+        try {
+            const existenErrores = await validate(numeroValido) //con el await siempre se utiliza el bloque try y catch
+            if(existenErrores.length > 0){
+                console.log('Errores: ',existenErrores);
+                throw new BadRequestException('Error validando');
+            } else {
+                const numero1 = Number(num1.primero);
+                const numero2 = Number(num2.segundo);
+                console.log(numero1)
+                console.log(numero2)
+                return "La resta de los dos números es: " + (numero1 - numero2);
+            }
+
+        }catch (e) {
+            console.error('Error', e);
+            throw new BadRequestException('Error validando');
+        }
+
+
+        //console.log('primero',num1.primero);
+        //console.log('segundo',num2);
+        //return 'Bienvenido resta, todo salió bien';
     }
 
-    @Delete('multiplicacion/:num2')
+    @Delete('multiplicacion/:segundo')
     @HttpCode(200)
     //@Header('primero', 'none')
-    multiplicacion(
+    async multiplicacion(
         @Headers() num1,
         @Param() num2
     ){
-        console.log('num2',num1);
-        console.log('num2',num2);
-        return 'Bienvenido multiplicacion, todo salió bien';
+
+        console.log("INICIA LA MULTIPLICACION")
+        const primero = Number(num1.primero)
+        //console.log(typeof(primero))
+        const segundo = Number(num2.segundo)
+
+        const numeroValido = new NumeroCreateDto();
+        numeroValido.numero1 = primero;
+        numeroValido.numero2 = segundo;
+
+        try {
+            const existenErrores = await validate(numeroValido) //con el await siempre se utiliza el bloque try y catch
+            if(existenErrores.length > 0){
+                console.log('Errores: ',existenErrores);
+                throw new BadRequestException('Error validando');
+            } else {
+                const numero1 = Number(num1.primero);
+                const numero2 = Number(num2.segundo);
+                console.log(numero1)
+                console.log(numero2)
+                return "La multiplicacion de los dos números es: " + (numero1 * numero2);
+            }
+
+        }catch (e) {
+            console.error('Error', e);
+            throw new BadRequestException('Error validando');
+        }
+
+        //console.log('num2',num1);
+        //console.log('num2',num2);
+        //return 'Bienvenido multiplicacion, todo salió bien';
     }
 
-    @Post('division/:num1')
+    @Post('division/:primero')
     @HttpCode(201)
-    division(
+    async division(
         @Param() num1,
         @Query() num2,
     ){
-        console.log('num1',num1);
-        console.log('num2',num2);
-        return 'Bienvenido division, todo salió bien';
+
+        console.log("INICIA LA MULTIPLICACION")
+        const primero = Number(num1.primero)
+        //console.log(typeof(primero))
+        const segundo = Number(num2.segundo)
+
+        const numeroValido = new NumeroCreateDto();
+        numeroValido.numero1 = primero;
+        numeroValido.numero2 = segundo;
+
+        try {
+            const existenErrores = await validate(numeroValido) //con el await siempre se utiliza el bloque try y catch
+            if(existenErrores.length > 0){
+                console.log('Errores: ',existenErrores);
+                throw new BadRequestException('Error validando');
+            } else {
+                const numero1 = Number(num1.primero);
+                const numero2 = Number(num2.segundo);
+                console.log(numero1)
+                console.log(numero2)
+                return "La division de los dos números es: " + (numero1 / numero2);
+            }
+
+        }catch (e) {
+            console.error('Error', e);
+            throw new BadRequestException('Error validando');
+        }
+
+        //console.log('num1',num1);
+        //console.log('num2',num2);
+        //return 'Bienvenido division, todo salió bien';
     }
 
 
---
+++ 02-Deber/deber/src/deber/dto/numero.create-dto.ts
@@ -0,0 +1,27 @@
+//@IsAlpha()
+//@IsNotEmpty()
+//@MinLength()
+//@MaxLength()
+//@IsBoolean()
+//@IsEmpty()
+//@IsInt()
+//@IsOptinonal()
+//@IsNumber()
+
+import {
+    IsNumber,
+    IsNotEmpty,
+    IsInt,
+    IsEmpty
+} from "class-validator";
+
+export class NumeroCreateDto {
+    @IsNotEmpty()
+    @IsNumber()
+    @IsInt()
+    numero1 : number;
+    @IsNotEmpty()
+    @IsNumber()
+    @IsInt()
+    numero2: number;
+}
